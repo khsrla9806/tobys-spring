1a375f0ce06f45327c25537f04419019ccac36ac
khsrla9806 <kimhunsope@naver.com>
2023-01-29 00:47:36 +0900

    애노테이션을 사용하여 DaoFactory 설정 파일로 만들기

--- src/main/java/org/example/user/dao/DaoFactory.java
+++ src/main/java/org/example/user/dao/DaoFactory.java
@@ -1,10 +1,16 @@
 package org.example.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
 
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new AConnectionMaker();
     }
