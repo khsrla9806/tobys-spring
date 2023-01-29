23d4f9ee4f728c004bf0609e7a42053a533c91a0
khsrla9806 <kimhunsope@naver.com>
2023-01-29 22:01:36 +0900

    CountingConnectionMaker 생성 후 테스트까지 구현

--- src/test/java/org/example/user/dao/UserDaoConnectionCountingTest.java
+++ src/test/java/org/example/user/dao/UserDaoConnectionCountingTest.java
@@ -6,13 +6,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import java.sql.SQLException;
 
-public class UserDaoTest {
+public class UserDaoConnectionCountingTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
         UserDao userDao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
-        user.setId("hoon2");
+        user.setId("hoon4");
         user.setName("훈훈");
         user.setPassword("123456789");
 
@@ -25,5 +25,8 @@ public class UserDaoTest {
         System.out.println(user2.getPassword());
 
         System.out.println(user2.getId() + " 조회 성공!");
+
+        CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
+        System.out.println("Connection Counter : " + ccm.getCounter());
     }
 }
