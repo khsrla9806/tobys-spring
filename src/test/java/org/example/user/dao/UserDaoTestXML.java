e9ac60f01916928587a442a4a7262b92e2e12596
khsrla9806 <kimhunsope@naver.com>
2023-01-29 22:59:04 +0900

    XML 설정파일을 이용하여 스프링 설정정보 만들기

---
+++ src/test/java/org/example/user/dao/UserDaoTestXML.java
@@ -0,0 +1,30 @@
+package org.example.user.dao;
+
+import org.example.user.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.sql.SQLException;
+
+public class UserDaoTestXML {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        UserDao userDao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("hoon6");
+        user.setName("훈훈");
+        user.setPassword("123456789");
+
+        userDao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공!");
+
+        User user2 = userDao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공!");
+    }
+}
