8338c1eacb3c7668d46bd95822754516f4e8c6ce
khsrla9806 <kimhunsope@naver.com>
2023-01-30 18:58:39 +0900

    Test에서 Application Context가 한번만 생성되는지 테스트

--- src/test/java/org/example/user/dao/JUnitTest.java
+++ src/test/java/org/example/user/dao/JUnitTest.java
@@ -1,6 +1,11 @@
 package org.example.user.dao;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -8,24 +13,39 @@ import java.util.Set;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/junit.xml")
 public class JUnitTest {
+    @Autowired
+    private ApplicationContext context;
+
     static Set<JUnitTest> testObject = new HashSet<>();
+    static ApplicationContext contextObject = null;
 
     @Test
     public void test1() {
         assertThat(testObject, is(not(hasItem(this))));
         testObject.add(this);
+
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
     }
 
     @Test
     public void test2() {
         assertThat(testObject, is(not(hasItem(this))));
         testObject.add(this);
+
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
     }
 
     @Test
     public void test3() {
         assertThat(testObject, is(not(hasItem(this))));
         testObject.add(this);
+
+        assertThat(contextObject, either(nullValue()).or(is(this.context)));
+        contextObject = this.context;
     }
 }
