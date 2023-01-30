c754983c73617f10eaea15d64ceab4fd02444b87
khsrla9806 <kimhunsope@naver.com>
2023-01-30 15:16:10 +0900

    getCount() 메서드에 대한 테스트 코드 작성

--- src/test/java/org/example/user/dao/UserDaoTest.java
+++ src/test/java/org/example/user/dao/UserDaoTest.java
@@ -30,4 +30,26 @@ public class UserDaoTest {
         assertThat(user2.getName(), is(user.getName()));
         assertThat(user2.getPassword(), is(user.getPassword()));
     }
+
+    @Test
+    public void count() throws SQLException {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("user1", "유저1", "1234");
+        User user2 = new User("user2", "유저2", "1234");
+        User user3 = new User("user3", "유저3", "1234");
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.add(user1);
+        assertThat(dao.getCount(), is(1));
+
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
+
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
+    }
 }
