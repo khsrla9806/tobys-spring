14f068690c698e28befa5598a02bc66d8d037dc2
khsrla9806 <kimhunsope@naver.com>
2023-02-08 18:29:04 +0900

    Level enum을 정의하고, User 클래스에 필드 추가하기

--- src/main/java/org/example/user/domain/User.java
+++ src/main/java/org/example/user/domain/User.java
@@ -1,6 +1,10 @@
 package org.example.user.domain;
 
 public class User {
+    Level level;
+    int login;
+    int recommend;
+
     String  id;
     String name;
     String password;
@@ -14,6 +18,30 @@ public class User {
     public User() {
         // 자바빈 규약을 따라는 클래스에 생성자를 명시적으로 추가했을 때
         // 파라미터가 없는 디폴트 생성자도 함께 정의해주는 것을 잊지 말자.
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    public int getLogin() {
+        return login;
+    }
+
+    public void setLogin(int login) {
+        this.login = login;
+    }
+
+    public int getRecommend() {
+        return recommend;
+    }
+
+    public void setRecommend(int recommend) {
+        this.recommend = recommend;
     }
 
     public String getId() {
