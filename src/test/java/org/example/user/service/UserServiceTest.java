dff2bafc7a15489ef23dc2e68299792a1f0505b3
khsrla9806 <kimhunsope@naver.com>
2023-02-09 22:57:09 +0900

    사용자 레벨 업그레이드 테스트 코드 작성

--- src/test/java/org/example/user/service/UserServiceTest.java
+++ src/test/java/org/example/user/service/UserServiceTest.java
@@ -1,10 +1,18 @@
 package org.example.user.service;
 
+import org.example.user.dao.UserDao;
+import org.example.user.domain.Level;
+import org.example.user.domain.User;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
@@ -15,4 +23,41 @@ import static org.junit.Assert.assertThat;
 public class UserServiceTest {
     @Autowired
     UserService userService;
+
+    @Autowired
+    UserDao userDao;
+
+    List<User> users;
+
+    @Before
+    public void setUp() {
+        users = Arrays.asList(
+                new User("hoon", "훈", "p1234", Level.BASIC, 49, 0),
+                new User("you", "유", "p1234", Level.BASIC, 50, 0),
+                new User("min", "민", "p1234", Level.SILVER, 60, 29),
+                new User("young", "영", "p1234", Level.SILVER, 60, 30),
+                new User("sun", "선", "p1234", Level.GOLD, 100, 100)
+        );
+    }
+
+    @Test
+    public void upgradeLevels() {
+        userDao.deleteAll();
+        for (User user : users) {
+            userDao.add(user);
+        }
+
+        userService.upgradeLevels();
+
+        checkLevel(users.get(0), Level.BASIC);
+        checkLevel(users.get(1), Level.SILVER);
+        checkLevel(users.get(2), Level.SILVER);
+        checkLevel(users.get(3), Level.GOLD);
+        checkLevel(users.get(4), Level.GOLD);
+    }
+
+    public void checkLevel(User user, Level expectedLevel) {
+        User userUpdate = userDao.get(user.getId());
+        assertThat(userUpdate.getLevel(), is(expectedLevel));
+    }
 }
