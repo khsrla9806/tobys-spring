c6daf9f0546abc5b72e4edc6f40c00f47013ae72
khsrla9806 <kimhunsope@naver.com>
2023-02-09 22:45:48 +0900

    사용자 레벨 업그레이드 메서드

--- src/main/java/org/example/user/service/UserService.java
+++ src/main/java/org/example/user/service/UserService.java
@@ -1,11 +1,39 @@
 package org.example.user.service;
 
 import org.example.user.dao.UserDao;
+import org.example.user.domain.Level;
+import org.example.user.domain.User;
+
+import java.util.List;
 
 public class UserService {
     UserDao userDao;
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    public void upgradeLevels() {
+        List<User> users = userDao.getAll();
+
+        for (User user : users) {
+            Boolean changed = null; // 레벨의 변화를 해결하기 위한 변수
+
+            if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
+                user.setLevel(Level.SILVER);
+                changed = true;
+            } else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
+                user.setLevel(Level.GOLD);
+                changed = true;
+            } else if (user.getLevel() == Level.GOLD) {
+                changed = false;
+            } else {
+                changed = false;
+            }
+
+            if (changed) {
+                userDao.update(user); // 레벨 변경이 있다면 DAO의 update() 호출
+            }
+        }
     }
 }
