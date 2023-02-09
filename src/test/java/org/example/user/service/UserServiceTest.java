74d5ab965bb15c3854869dd646d740bdcc2d0e5b
khsrla9806 <kimhunsope@naver.com>
2023-02-09 22:37:06 +0900

    UserServiceTest 테스트 클래스 생성

--- src/test/java/org/example/user/service/UserServiceTest.java
+++ src/test/java/org/example/user/service/UserServiceTest.java
@@ -1,9 +1,14 @@
 package org.example.user.service;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/test-applicationContext.xml")
