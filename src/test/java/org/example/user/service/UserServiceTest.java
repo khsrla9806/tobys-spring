ed0be814a58b32e5014fe185875fb1ee125250f8
khsrla9806 <kimhunsope@naver.com>
2023-02-09 22:23:09 +0900

    UserServiceTest 클래스 생성

---
+++ src/test/java/org/example/user/service/UserServiceTest.java
@@ -0,0 +1,13 @@
+package org.example.user.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/test-applicationContext.xml")
+public class UserServiceTest {
+    @Autowired
+    UserService userService;
+}
