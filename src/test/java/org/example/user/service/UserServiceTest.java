5ca53e93dc48a2e3c149e20fa21b7fa118d18cdc
khsrla9806 <kimhunsope@naver.com>
2023-02-10 14:57:28 +0900

    UserService의 테스트용 대역 클래스/테스트용 예외

--- src/test/java/org/example/user/service/UserServiceTest.java
+++ src/test/java/org/example/user/service/UserServiceTest.java
@@ -86,4 +86,24 @@ public class UserServiceTest {
             assertThat(userUpdate.getLevel(), is(user.getLevel()));
         }
     }
+
+    static class TestUserService extends UserService {
+        private String id;
+
+        private TestUserService(String id) {
+            this.id = id;
+        }
+
+        @Override
+        protected void upgradeLevel(User user) {
+            if (user.getId().equals(this.id)) {
+                throw new TestUserServiceException();
+            }
+            super.upgradeLevel(user);
+        }
+    }
+
+    static class TestUserServiceException extends RuntimeException {
+
+    }
 }
