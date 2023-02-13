e4d30e32ce8a4f4130f5768c7b1cab7636b2f4b3
khsrla9806 <kimhunsope@naver.com>
2023-02-13 20:42:14 +0900

    스프링의 MailSender를 이용한 메일 발송 메서드

--- src/main/java/org/example/user/service/UserService.java
+++ src/main/java/org/example/user/service/UserService.java
@@ -3,6 +3,9 @@ package org.example.user.service;
 import org.example.user.dao.UserDao;
 import org.example.user.domain.Level;
 import org.example.user.domain.User;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
@@ -68,23 +71,16 @@ public class UserService {
     }
 
     private void sendUpgradeEmail(User user) {
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "mail.ksug.org");
-        Session s = Session.getInstance(props, null);
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl(); // 구현 클래스인 JavaMailSenderImpl
+        mailSender.setHost("mail.server.com");
 
-        MimeMessage message = new MimeMessage(s);
-        try {
-            message.setFrom(new InternetAddress("useradmin@ksug.org"));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
-            message.setSubject("Upgrade 안내");
-            message.setText("사용자님의 등급이 " + user.getLevel().name() + "으로 업그레이드되었습니다.");
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(user.getEmail());
+        mailMessage.setFrom("useradmin@sug.org");
+        mailMessage.setSubject("Upgrade 안내");
+        mailMessage.setText("사용자님의 등급이 " + user.getLevel().name());
 
-            Transport.send(message);
-        } catch (AddressException e) {
-            throw new RuntimeException(e);
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
+        mailSender.send(mailMessage);
     }
 
     public void add(User user) {
